eae5e2b18b0eba22300c78b7bff88ec1f8322ac5
Sergeanur <4450430+Sergeanur@users.noreply.github.com>
2023-10-24 19:29:09 +0300

    Remove unused code

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -42,30 +42,6 @@ void threadWaitingLoop(hook::pattern pattern)
     SetResolutionHook(pattern, newResolution);
 }
 
-
-std::wstring GetModuleFileNameW(HMODULE hModule)
-{
-    static constexpr auto INITIAL_BUFFER_SIZE = MAX_PATH;
-    static constexpr auto MAX_ITERATIONS = 7;
-    std::wstring ret;
-    auto bufferSize = INITIAL_BUFFER_SIZE;
-    for (size_t iterations = 0; iterations < MAX_ITERATIONS; ++iterations)
-    {
-        ret.resize(bufferSize);
-        auto charsReturned = GetModuleFileNameW(hModule, &ret[0], bufferSize);
-        if (charsReturned < ret.length())
-        {
-            ret.resize(charsReturned);
-            return ret;
-        }
-        else
-        {
-            bufferSize *= 2;
-        }
-    }
-    return L"";
-}
-
 std::wstring GetModulePath(HMODULE hModule)
 {
     std::wstring path;
